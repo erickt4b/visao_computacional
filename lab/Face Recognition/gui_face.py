4e81f3910f6cee38c962c2ed7532364783365736
erickt4b <53802321+erickt4b@users.noreply.github.com>
2020-05-03 00:42:08 -0300

    Add files via upload

---
+++ lab/Face Recognition/gui_face.py
@@ -0,0 +1,50 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+
+#import _tkinter as Tk
+import tkinter as tk
+import os
+
+
+# In[2]:
+
+
+root = tk.Tk(className = 'face_recognition_gui')
+svalue = tk.StringVar() # defines the widget state as string
+
+w = tk.Entry(root,textvariable=svalue) # adds a textarea widget
+w.pack()
+
+
+def train_fisher_btn_load():
+    name = svalue.get()
+    os.system('python face_train_fisher.py %s'%name)
+
+def train_eigen_btn_load():
+    name = svalue.get()
+    os.system('python face_train_eigen.py %s'%name)
+
+def recog_fisher_btn_load():
+    os.system('python face_recog_fisher.py')
+
+def recog_eigen_btn_load():
+    os.system('python face_recog_eigen.py')
+
+
+trainF_btn = tk.Button(root,text="Train (FisherFaces)", command=train_fisher_btn_load)
+trainF_btn.pack()
+
+recogF_btn = tk.Button(root,text="Recognize (FisherFaces)", command=recog_fisher_btn_load)
+recogF_btn.pack()
+
+trainE_btn = tk.Button(root,text="Train (EigenFaces)", command=train_eigen_btn_load)
+trainE_btn.pack()
+
+recogE_btn = tk.Button(root,text="Recognize (EigenFaces)", command=recog_eigen_btn_load)
+recogE_btn.pack()
+
+root.mainloop()
+
